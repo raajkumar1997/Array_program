b6bb1480fd0ac63e92e8730f649d148ea429befc
raajkumar1997 <graajkumar97@gmail.com>
2022-12-27 19:37:05 +0530

    second user

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"header.h"
+void main()
+{
+        printf("\nHello world main\n");
+        printf("\nEnter the hello\n");
+        hello();
+        printf("\nAdd the number\n");
+        add(3,5);
+        printf("\nThanksyou\n");
+
+}
